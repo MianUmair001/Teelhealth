e6369c5a077ee1008fe701052f9f0166cdd537bc
Muhammad Umair <Muhammad.Umair@soliton.local>
2022-06-22 14:33:05 +0500

    Init

---
+++ src/components/host/Login/Login.tsx
@@ -0,0 +1,13 @@
+import { Col, Row } from "antd";
+
+function Login() {
+  return (
+    <>
+      <Row>
+        <Col>Form</Col>
+        <Col>Image</Col>
+      </Row>
+    </>
+  );
+}
+export default Login
