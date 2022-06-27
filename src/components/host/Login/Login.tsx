4d7d3ac4282a5ed4f97e0b122a4e13edc014047c
Sohaib Amjad <Sohaib.Amjad@soliton.local>
2022-06-27 12:47:03 +0500

    Add images and fix login form issue

--- src/components/host/Login/Login.tsx
+++ src/components/host/Login/Login.tsx
@@ -1,28 +1,52 @@
 import { Row, Col, Card, Form, Input, Button, message } from "antd";
 import './Login.scss';
 import Logo from '../../../assets/images/persivia-logo.png';
-import LoginGraphic from '../../../assets/images/img-login.png';
+import LoginGraphic from '../../../assets/images/provider-login.svg';
 function Login() {
+
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
   return (
     <>
       <div className="ant-container">
         <Row align="middle" className="th-midbox">
           <Col md={12}>
-            <h1 className="h1">Welcome <span>to</span></h1>
+            <h1 className="h1">Welcome to</h1>
             <img src={Logo} width={140} /> <span className="logo-tagline">Telehealth</span>
             <br /><br />
             <div className="th-card-wrapper">
               <p>Sign in to your account</p>
               <Card className="th-card">
-                <Form layout="vertical" autoComplete="off" className="th-form">
-                  <Form.Item name="username" label="Username">
+                <Form onFinish={onFinish} onFinishFailed={onFinishFailed} layout="vertical" autoComplete="off" className="th-form">
+                  <span className="general-error" style={{color: "#E05A5A", fontWeight: "600", marginBottom: "10px", display: "block"}}>Login unsuccessful. Invalid username or password.</span>
+                  <Form.Item rules={[
+                    {
+                      type: 'email',
+                      message: 'The input is not valid E-mail!',
+                    },
+                    {
+                      required: true,
+                      message: 'Please input your E-mail!',
+                    },
+                  ]} name="username" label="Username">
                     <Input placeholder="John Doe" />
                   </Form.Item>
-                  <Form.Item name="password" label="Password">
-                    <Input placeholder="******" />
+                  <Form.Item rules={[
+                    {
+                      required: true,
+                      message: 'Please input your password!',
+                    },
+                  ]} name="password" label="Password">
+                    <Input.Password placeholder="******" />
                   </Form.Item>
                   <Form.Item>
-                    <Button htmlType="button" className="w-100">
+                    <Button htmlType="submit" className="w-100">
                       Sign In
                     </Button>
                   </Form.Item>
